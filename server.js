2d8d67535fe8563a78f3348bb0b92dfa10e3f4dd
Azacora <90911160+Azacora@users.noreply.github.com>
2021-09-17 09:44:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -51,10 +51,10 @@ const client = new Discord.Client();
 var telegram = require("natsvora-telegram-bot-api");
 
 // import env variables
-var telegramToken = process.env.AAEBS-ms7VPBCPVs7Me8YQefXJLAk_QjySw
-const DISCORD_TOKEN = process.env.ODg4NDE0OTEwNzA5NjM3MTUy.YUSW3A.n3usCuV65tJUOW5uFUZl_AbX8K4
-var telegramChatId = process.env.1556248388
-var discordChannelId = process.env.876984748881346591;
+var telegramToken = 2042732893:AAEBS-ms7VPBCPVs7Me8YQefXJLAk_QjySw
+const DISCORD_TOKEN = ODg4NDE0OTEwNzA5NjM3MTUy.YUSW3A.n3usCuV65tJUOW5uFUZl_AbX8K4
+var telegramChatId = -1556248388
+var discordChannelId = 876984748881346591;
 
 const webhookClient = new Discord.WebhookClient(
   process.env.888410163688661002,
